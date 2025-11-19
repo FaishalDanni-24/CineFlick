ce703b59f1425c43b9c3627de62d3016eca5b0cb
FaishalDanni-24 <faishaldanni24@gmail.com>
2025-11-19 19:53:26 +0700

    Mengembalikan halaman dashboard dan profile dari Laravel Breeze

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,19 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('home') }}">
+                        <div class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200">
+                            <img src="{{ asset('images/Logo_CineFlick_Small.png') }}" 
+                                alt="CineFlick Logo"
+                                class="h-10 object-contain">
+                        </div>
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Homepage') }}
                     </x-nav-link>
                 </div>
             </div>
