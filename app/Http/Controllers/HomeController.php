415061e671073f3a7b316a382eb43043ab282850
Muba-rakk <winatafadil1177@gmail.com>
2025-11-23 19:39:52 +0700

    Mengubah Controller untuk Homepage agar lebih optimal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\View\View;
+use App\Models\Film;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Display the homepage
-     */
-    public function index(): View
+    public function index(Request $request)
     {
-        return view('home');
+        $selectedGenre = $request -> input('genre');
+
+        $featuredFilms = Film::latest()
+            -> take(2)
+            -> get();
+
+        $genres = Film::select('genre')
+            -> distinct()
+            -> pluck('genre')
+            -> filter()
+            -> sort()
+            -> values();
+
+        $filmsQuery = Film::query();
+
+        if ($selectedGenre) {
+            $filmsQuery -> where('genre', $selectedGenre);
+        }
+
+        $films = $filmsQuery -> latest() -> get();
+
+        if ($request -> ajax()) {
+            return view('components.movies-grid', compact('films')) -> render();
+        }
+
+        return view('home', compact('featuredFilms', 'genres', 'films', 'selectedGenre'));
     }
 }
